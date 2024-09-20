eded3ec4e8bbfb533687a05af48b5757c2772e7a
fweigel-dev <fabian.weigel1@web.de>
2024-09-20 13:52:51 +0200

    :memo: update minor and major gc slude to use HashSet instead of HashMap

--- Java-Garbage-Collection-Basics-II/MinorAndMayorCollection.java
+++ Java-Garbage-Collection-Basics-II/MinorAndMayorCollection.java
@@ -1,23 +1,23 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
-// Code Example from https://www.baeldung.com/java-verbose-gc
+// Adapted Code Example from https://www.baeldung.com/java-verbose-gc
 public class MinorAndMayorCollection {
 
-    private static final Map<String, String> stringContainer = new HashMap<>();
+    private static final Set<String> stringContainer = new HashSet<>();
 
     public static void main(String[] args) {
         String stringWithPrefix = "stringWithPrefix";
 
-        for (int i = 0; i < 3000000; i++) {
+        for (int i = 0; i < 3_000_000; i++) {
             String newString = stringWithPrefix + i;
-            stringContainer.put(newString, newString);
+            stringContainer.add(newString);
         }
         System.out.println("MAP size: " + stringContainer.size());
 
         System.gc();
 
-        for (int i = 0; i < 2000000; i++) {
+        for (int i = 0; i < 2_000_000; i++) {
             String newString = stringWithPrefix + i;
             stringContainer.remove(newString);
         }
