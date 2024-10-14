6669faf04feb2ba075dc929d64dec004118fff68
fweigel-dev <fabian.weigel1@web.de>
2024-10-14 17:29:57 +0200

    :memo: Update presentation

---
+++ Java-Vererbung/AbstractBeispiel.java
@@ -0,0 +1,12 @@
+public class AbstractBeispiel {
+    public static void main(String[] args) {
+        /* Zeile 3-6 auskommentieren, um Fehlermeldung zu sehen
+        Tier tier = new Tier(); // -> java: Tier is abstract; cannot be instantiated
+        tier.essen();
+         */
+    }
+}
+
+/*
+    Abstrakte Klassen können nicht instanziiert werden und können nur als Basisklassen für andere Klassen verwendet werden!
+ */
---
+++ Java-Vererbung/Beispiel.java
@@ -0,0 +1,7 @@
+public class Beispiel {
+    public static void main(String[] args) {
+        Hund hund = new Hund();
+        hund.essen();
+        hund.bellen();
+    }
+}
